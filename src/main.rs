0249aecdbebe2e4699278d72ee23000709c658d6
Rob Wagner <rob@sombia.com>
2024-01-02 20:46:56 -0500

    Update vendored htmx version

--- src/main.rs
+++ src/main.rs
@@ -43,14 +43,12 @@ pub fn leak_alloc<T>(value: T) -> &'static T {
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
-    dotenvy::dotenv().ok();
-
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
         .with(EnvFilter::from_default_env())
         .init();
 
-    let config = Config::new();
+    let config = Config::new(".env");
 
     let rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
